4b3886293a26c5fdf211abcf8e3f6192f931c84c
Poonamjnvu <Poonamjnvu>
2022-04-20 11:26:01 +0530

    add file

---
+++ index.js
@@ -0,0 +1,93 @@
+console.log("Working");
+
+//Q1.
+function counter(){
+    var counter = 0;
+
+    function IncreaseCounter() {
+        return counter += 1;
+    };
+
+    return IncreaseCounter;
+}
+
+var counter = counter();
+alert(counter());
+alert(counter());
+alert(counter());
+alert(counter());
+
+
+//Q2.
+let count = 0;
+(function immediate() {
+  if (count === 0) {
+    let count = 1;
+    console.log(count); // What is logged?
+  }
+  console.log(count); // What is logged?
+})();
+
+
+//Q3.
+for (var i = 0; i < 3; i++) {
+    setTimeout(function log() {
+      console.log(i); // What is logged?
+    }, 1000);
+  }
+
+  Q4.
+let l = 7;
+function rectangle(){
+    let b = 8;
+    console.log("Area of rectangle is ",l*b);
+}
+rectangle();
+
+
+//Q5.
+let count = 0;
+function Counter1(){
+    for(let i=1;i<=10;i++){
+        count+=1;
+    }
+    console.log(count);
+}
+Counter1();
+
+//Q6.
+var a = 12;
+(function () {
+  alert(a);
+})();
+
+
+//Q7.
+var a = 10;
+var x = (function () {
+  var a = 12;
+  return function () {
+    alert(a);
+  };
+})();
+x();
+
+
+//Q8.
+var globalVar = "xyz";
+
+(function outerFunc(outerArg) {
+    var outerVar = 'a';
+    
+    (function innerFunc(innerArg) {
+    var innerVar = 'b';
+    
+    console.log(
+        "outerArg = " + outerArg + "\n" +
+        "innerArg = " + innerArg + "\n" +
+        "outerVar = " + outerVar + "\n" +
+        "innerVar = " + innerVar + "\n" +
+        "globalVar = " + globalVar);
+    
+    })(456);
+})(123);
